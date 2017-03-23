add1da74f1835fbee7fa71b0333dee4b67b393c5
Oliver Lehner <lehner.oliver@gmail.com>
2017-03-23 17:30:36 +0100

    seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,14 +6,13 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-# Ingredient.create(name: "lemon")
-# Ingredient.create(name: "ice")
-# Ingredient.create(name: "mint leaves")
-# Ingredient.create(name: "whiskey")
-# Ingredient.create(name: "vodka")
-# Ingredient.create(name: "ice cubes")
-# Ingredient.create(name: "gin")
-# Ingredient.create(name: "tonic")
+Ingredient.create(name: "lemon")
+Ingredient.create(name: "ice")
+Ingredient.create(name: "mint leaves")
+Ingredient.create(name: "whiskey")
+Ingredient.create(name: "vodka")
+Ingredient.create(name: "ice cubes")
+Ingredient.create(name: "gin")
+Ingredient.create(name: "tonic")
 
 
-# Cocktail.create(name: "gin tonic")
