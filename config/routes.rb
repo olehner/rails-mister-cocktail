a88c9ca53aac629848d5a0984321987af89411ab
Oliver Lehner <lehner.oliver@gmail.com>
2017-03-23 17:04:55 +0100

    at least it's green

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,8 @@ Rails.application.routes.draw do
 resources :cocktails do
   resources :doses, only: [:index, :new, :create]  
 end
-resources :doses, only: [:destroy]
+resources :doses, only: [:destroy, :show]
 
+root 'cocktails#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
